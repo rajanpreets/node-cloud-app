7e546046b5795eb4eec10f9b8d7c33b92e730d9b
RAJAN <rajanpreet8singh@gmail.com>
2025-03-01 14:57:22 +0530

    app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ load_dotenv()
 GROQ_API_KEY = os.getenv("GROQ_API_KEY")
 PINECONE_API_KEY = os.getenv("PINECONE_API_KEY")
 PINECONE_ENV = os.getenv("PINECONE_ENV")
-PINECONE_INDEX_NAME = "rajan"
+INDEX_NAME = "rajan"
 
 # Initialize Pinecone
 pc = Pinecone(api_key=PINECONE_API_KEY)
